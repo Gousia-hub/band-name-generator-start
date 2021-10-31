951f9e330b61daa0c2e64824101ac3fb4538206f
pythongousia <84321414+Gousia-hub@users.noreply.github.com>
2021-10-31 04:12:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Hello Everyone. Welcome to our new Artist Band.")
+#2. Ask the user for the city that they grew up in.
+paint = input("What is your favourite type of painting?\n")
+
+#3. Ask the user for the name of a pet.
+fan = input("What is your favourite fantasy object?\n")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = paint + " " + fan
+print("Your new Band name is:" +  band_name)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
